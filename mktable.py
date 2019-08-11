8cd06ff1e15961648ea7f868f68fa5f7ec6dacc4
quarterturn <github@matrixwide.com>
2019-08-11 09:17:57 -0400

    Updated wavetable 2

--- mktable.py
+++ mktable.py
@@ -6,7 +6,7 @@ my_index = 0
 my_range = 75 
 
 for my_index in range(0, my_samples):
-  y = (((sin(((2.0 * pi)/my_samples) * my_index)) * my_range) / 2.0) + (((sin(((20.0 * pi)/my_samples) * my_index)) * my_range) / 4.0)
+  y = (((sin(((2.0 * pi)/my_samples) * my_index)) * my_range) / 2.0) + (((sin(((20.0 * pi)/my_samples) * my_index)) * my_range) / 4.7)
 #  y = ((sin(((2.0 * pi)/my_samples) * my_index)) * my_range)
   print('%d,' %  round(y))
 
